5bd1f5911e7031343a8ae15f204b944dee6cec62
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-07 16:18:40 +0400

    feature:identity package added

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers;
+
+public class AccountController:Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    } 
+
+    public IActionResult Login()
+    {
+        return View();
+    } 
+}
