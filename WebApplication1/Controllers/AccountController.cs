3f71a89f6246385357f74b4c2bf0888efe93d910
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-09 15:36:40 +0400

    feature:Register Post View added,Identity migration added

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -1,12 +1,45 @@
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 namespace WebApplication1.Controllers;
 
 public class AccountController:Controller
 {
+    private readonly  UserManager<AppUser> _userManager;
+
+    public AccountController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
     public IActionResult Register()
     {
+        // AppUser user = new();
+
         return View();
     } 
+    
+    [HttpPost]
+    [AutoValidateAntiforgeryToken]
+    public async Task<IActionResult> Register (RegisterVm register)
+    {
+        if(!ModelState.IsValid) return View();
+        AppUser user = new();
+        user.Email=register.Email;
+        user.Fullname=register.Fullname;
+        user.UserName=register.Username;
+        IdentityResult result = await _userManager.CreateAsync(user,register.Password);
+        if(!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("",error.Description);
+            }
+            return View(register);
+        }
+        return View();
+    }
 
     public IActionResult Login()
     {
